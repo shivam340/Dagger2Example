12d033979d6e350fb59d14a9a019ae3a82b228ba
shivam340 <shivam@leftshift.io>
2015-10-05 13:36:14 +0530

    registered GreetFragment in component

--- app/src/main/java/test/com/daggerexample/daggerStuff/ApplicationComponent.java
+++ app/src/main/java/test/com/daggerexample/daggerStuff/ApplicationComponent.java
@@ -3,6 +3,7 @@ package test.com.daggerexample.daggerStuff;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import test.com.daggerexample.GreetFragment;
 import test.com.daggerexample.MainActivity;
 import test.com.daggerexample.model.UserDetails;
 
@@ -14,5 +15,7 @@ import test.com.daggerexample.model.UserDetails;
 @Component( modules = ApplicationModule.class )
 public interface ApplicationComponent {
     void inject(MainActivity mainActivity);
+    void inject(GreetFragment greetFragment);
+
     UserDetails getUser();
 }
